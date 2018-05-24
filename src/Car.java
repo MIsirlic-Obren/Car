16f59cdcbcb7e00a5473219f610c579f9e1768f1
Obren <obimisirlic@gmail.com>
2018-05-24 20:27:28 +0200

    putovanje

--- src/Car.java
+++ src/Car.java
@@ -10,7 +10,18 @@ public class Car {
     double currentPrice = 3000;
     int mileagePassed = 50000;
     
-    public Car() {}
+    String model;
+    int maxFuel;
+    int currentFuel;
+    int consumption;
+    
+    public Car() {
+    this.model = "default";
+    this.currentFuel = 0;
+    this.maxFuel =100 ;
+    }
+    
+  
     
     public Car(int maxSpeed, int minSpeed, double weight,boolean isTheCarOn, 
             String licence, char condition, double currentPrice, int mileagePassed){
@@ -25,5 +36,51 @@ public class Car {
         
     }
     
+    public void printMainAttributes() {
+        System.out.println("Model" + this.model);
+        System.out.println("Kapacitet rezervoara" + this.maxFuel);
+        System.out.println("Trenutno gorivo:" + this.currentFuel);
+        System.out.println("Pređena kilometraža:" + this.mileagePassed);
+        System.out.println("Potrosnja " + this.consumption);
+        System.out.println(" ");
+    }
+    public void printAttributes() {
+        System.out.println("Maksimalna brzina je" + this.maxSpeed);
+         System.out.println("Minimalna brzina je" + this.minSpeed);
+          System.out.println("Registracioni broj je" + this.licence);
+           System.out.println("Težina:" + this.weight);
+           System.out.println("Trenutna cena:" + this.currentPrice);
+    }
+    
+    public void changeModel(String customModel) {
+    this.model= customModel;
+    }
+    
+    public void changeMaxFuel(int customMaxFuel) {
+    this.maxFuel = customMaxFuel;
+    }
+    
+    public void fuelUp() {
+    this.currentFuel = this.maxFuel;
+    }
+    
+    public void changeConsumption (int customConsumption) {
+           this.consumption = customConsumption ;    
+    }
+    
+    
+    
+    public void travel (int distance) {
+        
+        if (this.currentFuel > distance * this.consumption) {
+            this.mileagePassed = this.mileagePassed + distance;
+            this.currentFuel = this.currentFuel - distance * this.consumption;
+        } else {
+            System.out.println("Nema dovoljno goriva");
+        
+        }
+        
+    
+    }
     
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,29 @@ public class Main {
     public static void main(String[] args) {
         
         Car myCar = new Car();
-        System.out.println("Maksimalna brzina je: " + myCar.maxSpeed);
+        myCar.printMainAttributes();
         
         Car companyCar = new Car (150, 30, 2000, false, "EFG-456", 'B', 3500, 60000);
-        System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed);
+        companyCar.changeModel("Fiat");
+        companyCar.changeMaxFuel(150);
+        companyCar.fuelUp();
+        companyCar.changeConsumption(10);
+        
+        companyCar.printMainAttributes();
+        companyCar.travel(20);
+        companyCar.printMainAttributes();
       
+        
+        Car audi = new Car();
+        audi.changeModel("Audi");
+        audi.fuelUp();
+        audi.changeConsumption(20);
+        
+        audi.printMainAttributes();
+        audi.travel(3);
+        audi.printMainAttributes();
+        
+        
     }
     
 }
